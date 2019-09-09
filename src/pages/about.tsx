512d4dfd27fa649e1caa8df3c3ce9cc8bfe74bb9
BailyTroyer <bailytroyer@gmail.com>
2019-09-09 13:44:33 -0400

    Changing job status and redirects

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,7 +16,7 @@ export default () => (
       heading="Who I am"
       description={(
         <>
-          I'm a Senior studying CSE at the <a href="http://engineering.buffalo.edu/computer-science-engineering.html" target="_blank" rel="noopener noreferrer">University at Buffalo</a> and an SRE intern at <a href="https://www.acvauctions.com/" target="_blank" rel="noopener noreferrer">ACV Auctions</a>.
+          I'm a Senior studying CSE at the <a href="http://engineering.buffalo.edu/computer-science-engineering.html" target="_blank" rel="noopener noreferrer">University at Buffalo</a> and a Software Engineer at <a href="https://www.acvauctions.com/" target="_blank" rel="noopener noreferrer">ACV Auctions</a>.
           My most recent side-project is Cachr and I really like making reliable and deployable systems. On the side, I like trying
           new restaurants and walking around the city!
         </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,14 +11,14 @@ export default () => (
 
     <Intro>
       Hi I'm Baily!
-      I'm a student at <Highlight>UB</Highlight>,&nbsp; and a Site Reliability Engineering Intern at <Highlight>ACV Auctions</Highlight>! My favorite tools and languages are <Highlight>Kubernetes</Highlight>, <Highlight>gRPC</Highlight>, <Highlight>Golang</Highlight> and <Highlight>Swift</Highlight>😃!
+      I'm a student at <Highlight>UB</Highlight>,&nbsp; and a Software Engineer at <Highlight>ACV Auctions</Highlight>! My favorite tools and languages are <Highlight>Kubernetes</Highlight>, <Highlight>gRPC</Highlight>, <Highlight>Golang</Highlight> and <Highlight>Swift</Highlight>😃!
     </Intro>
 
     <BlockText
       heading="Who I am"
       description={(
         <>
-          I'm a Senior studying CSE at the <a href="http://engineering.buffalo.edu/computer-science-engineering.html" target="_blank" rel="noopener noreferrer">University at Buffalo</a> and an SRE intern at <a href="https://www.acvauctions.com/" target="_blank" rel="noopener noreferrer">ACV Auctions</a>.
+          I'm a Senior studying CSE at the <a href="http://engineering.buffalo.edu/computer-science-engineering.html" target="_blank" rel="noopener noreferrer">University at Buffalo</a> and an Software Engineer at <a href="https://www.acvauctions.com/" target="_blank" rel="noopener noreferrer">ACV Auctions</a>.
           My most recent side-project is Cachr and I'm pushing myself to make posts on Medium. On the side, I like trying
           new restaurants and walking around the city!
         </>
